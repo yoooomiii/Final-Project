7ce598bfa5b05eb001bfeea6b74756d92ba3d5fd
Kimeojin <eojin1914@gmail.com>
2024-06-11 18:38:51 +0900

    logincontroller session

--- src/main/java/www/egg/hom/LoginController.java
+++ src/main/java/www/egg/hom/LoginController.java
@@ -1,6 +1,7 @@
 package www.egg.hom;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,13 +32,38 @@ public class LoginController {
 	}
 	@RequestMapping(value = "signIn", method = RequestMethod.GET) // 로그인 요청 (인터셉트에서 한번 걸러지고 난 뒤임)
 	public String signIn(@RequestParam("id") String id,
-			@RequestParam("pw") String pw) {
+			@RequestParam("pw") String pw, HttpSession session) {
 		// return "login/welcome";
 		// System.out.println("id/pw: "+id+"/"+pw);
+		// System.out.println("홈단 mvo: "+mvo);
 		
-		MemberVO mvo = lservice.signIn(id);
-		System.out.println("홈단 mvo: "+mvo);
-		return "redirect:loginForm";
+		MemberVO mvo = lservice.signIn(id); // request한 id로 사용자 정보를 담아 옴 
+		if(mvo  != null) { // 사용자 정보가 존재하면 
+			if(mvo.getPw().equals(pw)) { // request한 비번과 가져온 사용자 정보의 비번이 일치하면 
+				if(session.getAttribute("userid")!=null) { // session에 이미 담긴 값이 있다면 깨끗이 청소
+					session.removeAttribute("userid");
+					session.removeAttribute("username");
+					session.removeAttribute("useremail");
+					session.removeAttribute("useraddress");
+					session.removeAttribute("usergrade");
+				}
+				session.setAttribute("userid", mvo.getId()); // session에 가져온 사용자 정보 박기 
+				session.setAttribute("username", mvo.getName());
+				session.setAttribute("useremail", mvo.getEmail());
+				session.setAttribute("useraddress", mvo.getAddress());
+				session.setAttribute("usergrade", mvo.getMaster());
+			}else { // request가 잘못된 혹은 없는 비번을 줬을 때 
+				
+			}
+		}
+		/* 세션에 값이 제대로 저장되었는지 확인
+		 * System.out.println("session.getAttribute: "+session.getAttribute("userid"));
+		 * System.out.println("session.getAttribute: "+session.getAttribute("username"))
+		 * ;
+		 */
+		return "login/welcome";
+		
+	
 	}
 	@RequestMapping(value = "myPage", method = RequestMethod.GET) // 회원만이 접근할 수 있는 페이지라는 설정...
 	public String myPage() {
