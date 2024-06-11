c169af8bb09c0ff4a44a29078c53d09b061abde6
Kimeojin <eojin1914@gmail.com>
2024-06-11 11:53:11 +0900

    simple jsp

--- src/main/java/www/egg/hom/LoginController.java
+++ src/main/java/www/egg/hom/LoginController.java
@@ -1,12 +1,16 @@
 package www.egg.hom;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class LoginController {
 	// 집가서 자고 싶다.
 	
-	@GetMapping("/loginForm")
+	@RequestMapping(value = "loginForm", method = RequestMethod.GET)
 	public String loginForm() {
-		return "login";
+		return "loginF";
 	}
 }
