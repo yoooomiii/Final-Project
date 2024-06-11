6c4ac97c5ed220545b820855bf0ea1e50b4bbc48
jasmine-dg <jasmine_design@naver.com>
2024-06-11 17:22:28 +0900

    0611_j

--- src/main/java/www/egg/service/IF_MenuService.java
+++ src/main/java/www/egg/service/IF_MenuService.java
@@ -6,14 +6,4 @@ import www.egg.vo.MenuVO;
 
 public interface IF_MenuService {
 	
-	public void insert(MenuVO mvo) throws Exception;	//입력
-	
-	public List<MenuVO> menuList() throws Exception;	//전체보기
-	
-	public List<String> getFilename(String no) throws Exception;	//사진 드록
-	
-	public void delete(MenuVO mvo) throws Exception;	//삭제
-	
-	public void update(MenuVO mvo) throws Exception;	//수정
-
 }
--- src/main/java/www/egg/service/MenuServiceImpl.java
+++ src/main/java/www/egg/service/MenuServiceImpl.java
@@ -10,39 +10,6 @@ import www.egg.dao.IF_MenuDAO;
 import www.egg.vo.MenuVO;
 
 @Service
-public class MenuServiceImpl implements IF_MenuService {
+public class MenuServiceImpl {
 	
-	@Inject
-	IF_MenuDAO mdao;
-
-	@Override
-	public void insert(MenuVO mvo) throws Exception {	//입력
-		// TODO Auto-generated method stub
-		mdao.insert(mvo);
-	}
-
-	@Override
-	public List<MenuVO> menuList() throws Exception {	//전체보기
-		// TODO Auto-generated method stub
-		return mdao.menuList();
-	}
-
-	@Override
-	public List<String> getFilename(String no) throws Exception {	//사진 등록
-		// TODO Auto-generated method stub
-		return mdao.getFilename(no);
-	}
-
-	@Override
-	public void delete(MenuVO mvo) throws Exception {	//삭제
-		// TODO Auto-generated method stub
-		mdao.delete(mvo);
-	}
-
-	@Override
-	public void update(MenuVO mvo) throws Exception {	//수정
-		// TODO Auto-generated method stub
-		mdao.update(mvo);
-	}
-
 }
