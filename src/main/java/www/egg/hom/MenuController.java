ca4933c69c021153ad6eb62f0d4d2095b6cf2c34
Hongseongwo <ds1seongwoo@naver.com>
2024-06-19 18:47:51 +0900

    0619_hong

--- src/main/java/www/egg/hom/MenuController.java
+++ src/main/java/www/egg/hom/MenuController.java
@@ -16,9 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import www.egg.service.IF_MenuService;
 import www.egg.util.FileDataUtil;
-import www.egg.vo.ItemVO;
 import www.egg.vo.MenuVO;
-import www.egg.vo.OptionVO;
 
 @Controller
 public class MenuController {
@@ -29,103 +27,17 @@ public class MenuController {
 	@Inject
 	FileDataUtil filedatautil;
 	
-//	@GetMapping(value ="/mviewDetail")
-//	public String menuKeep(@RequestParam("menu_no") String no, String ono,
-//			OptionVO ovo, Model model) throws Exception {		//메뉴선택후 장바구니 과정 옵션 선택
-//		MenuVO mvo = mservice.modno(no);
-//		List<String> attackList = mservice.getFilename(no);
-//		model.addAttribute("mvo", mvo);
-//		model.addAttribute("attackList", attackList);
-//		
-//		List<String> oattackList = mservice.option_getFilename(ono);
-//		List<OptionVO> allList = mservice.option_List();
-//		model.addAttribute("optionList", allList);
-//		model.addAttribute("oattackList", oattackList);
-//		return "menu/menuKeep";
-//	}
 	
 	@GetMapping(value ="/Detail")
-	public String item_input(@RequestParam("menu_no") String no, @RequestParam("side_no") String ono, 
-			Model model) throws Exception {	//장바구니 입력 창으로 이동
+	public String item_Detail(@RequestParam("menu_no") String no,
+			Model model) throws Exception {		//선택한 치킨 정보 보기
 		MenuVO mvo = mservice.modno(no);
+		List<MenuVO> List = mservice.menuList();
+		model.addAttribute("List", List);
 		model.addAttribute("mvo", mvo);
-		OptionVO ovo = mservice.option_modno(ono);
-		model.addAttribute("ovo", ovo);
-		
 		
 		return "menu/itemInput";
 	}
-	
-	@PostMapping(value ="/item_inputSave")
-	public String item_input(@ModelAttribute ItemVO ivo) throws Exception {	//장바구니 등록 과정
-		
-		mservice.item_insert(ivo);
-		return null;
-	}
-	
-	//---------------------------------------------사이드
-
-	@GetMapping(value ="/option_input")
-	public String option_input() {	//옵션 입력 창으로 이동
-		
-		return "menu/optionInput";
-	}
-
-	@PostMapping(value ="/option_inputSave")
-	public String option_input(@ModelAttribute OptionVO ovo,
-			MultipartFile[] file) throws Exception {	//옵션 등록 과정
-		String[] filename = filedatautil.fileUpload(file);
-
-		for(int i=0; i<filename.length; i++) {
-			System.out.println(filename[i]);
-		}
-		ovo.setFilename(filename);
-		mservice.option_insert(ovo);
-
-		return "redirect:option_List";
-	}
-
-	@GetMapping(value ="/oviewDetail")
-	public String option_viewDetail(@RequestParam("side_no") String sno,
-			Model model) throws Exception { //선택한 사이드 메뉴 정보 보기
-		OptionVO ovo = mservice.option_modno(sno);
-		List<String> attackList = mservice.option_getFilename(sno);
-		model.addAttribute("ovo", ovo);
-		model.addAttribute("attackList", attackList);
-		return "menu/optionPick";
-	}
-
-	@RequestMapping("/option_List")
-	public String option_allList(@ModelAttribute OptionVO ovo,
-			Model model) throws Exception {	//옵션 전체보기
-		List<OptionVO> allList = mservice.option_List();
-		model.addAttribute("optionList", allList);
-
-		return "menu/optionList";
-	}
-
-	@PostMapping(value = "/option_delete")
-	public String option_delete(@ModelAttribute OptionVO ovo) throws Exception {	//등록한 옵션 삭제 과정
-		mservice.option_delete(ovo);
-
-		return "redirect:option_List";
-	}
-
-	@GetMapping(value ="/option_update")
-	public String option_up(@ModelAttribute OptionVO ovo, Model model) {	//옵션 수정 창으로 이동
-		model.addAttribute("ovo", ovo);
-
-		return "menu/optionUpdate";
-	}
-
-	@PostMapping(value ="/option_updateset")
-	public String option_update(@ModelAttribute OptionVO ovo) throws Exception {	//옵션 수정 동작 과정
-		mservice.option_update(ovo);
-
-		return "redirect:option_List";
-	}
-	
-	//----------------------------------------------------------------------메뉴
 
 	@GetMapping(value ="/menu_input")
 	public String menu_input() {		//치킨 입력 창으로 이동
@@ -145,14 +57,7 @@ public class MenuController {
 		mservice.insert(mvo);
 		return "redirect:menu_List";
 	}
-	
-	/*
-	 * @GetMapping(value ="/") public String main() { //메인 창으로 이동
-	 * 
-	 * return "main"; }
-	 */
 	 
-	
 	@GetMapping(value ="/menuForm")
 	public String menu() {		//메뉴 창으로 이동
 
@@ -171,17 +76,15 @@ public class MenuController {
 	}
 	
 	@GetMapping(value ="/mviewDetail")
-	public String menuKeep(@RequestParam("menu_no") String no, String ono,
-			OptionVO ovo, Model model) throws Exception {		//메뉴선택후 장바구니 과정 옵션 선택
+	public String menuKeep(@RequestParam("menu_no") String no,
+			Model model) throws Exception {		//메뉴선택후 장바구니 과정 옵션 선택
 		MenuVO mvo = mservice.modno(no);
 		List<String> attackList = mservice.getFilename(no);
+		List<MenuVO> List = mservice.menuList();
+		model.addAttribute("List", List);
 		model.addAttribute("mvo", mvo);
 		model.addAttribute("attackList", attackList);
 		
-		List<String> oattackList = mservice.option_getFilename(ono);
-		List<OptionVO> allList = mservice.option_List();
-		model.addAttribute("optionList", allList);
-		model.addAttribute("oattackList", oattackList);
 		return "menu/menuKeep";
 	}
 	
--- src/main/java/www/egg/service/IF_MenuService.java
+++ src/main/java/www/egg/service/IF_MenuService.java
@@ -5,42 +5,21 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import www.egg.vo.ItemVO;
 import www.egg.vo.MenuVO;
-import www.egg.vo.OptionVO;
 
 @Service
 public interface IF_MenuService {
-	
-	public void item_insert(ItemVO ivo) throws Exception;	//장바구니 등록
-	
-	//--------------------------------------------------장바구니
 
-	public void option_insert(OptionVO ovo) throws Exception;	//�궗�씠�뱶 �엯�젰
+	public void insert(MenuVO mvo) throws Exception;	//메뉴 입력
 
-	public List<OptionVO> option_List() throws Exception;	//�궗�씠�뱶 �쟾泥대낫湲�
+	public List<MenuVO> menuList() throws Exception;	//메뉴 전체보기
 
-	public void option_delete(OptionVO ovo) throws Exception;	//�궗�씠�뱶 �궘�젣
+	public void delete(MenuVO mvo) throws Exception;	//메뉴 삭제
 
-	public OptionVO option_update(OptionVO ovo) throws Exception;	//�궗�씠�뱶 �닔�젙
+	public MenuVO update(MenuVO mvo) throws Exception;	//메뉴 수정
 
-	public OptionVO option_modno(String no) throws Exception;	//�궗�씠�뱶 �젙蹂� �꽔湲�
+	public MenuVO modno(String no) throws Exception;	//메뉴 정보 넣기
 
-	public List<String> option_getFilename(String sno) throws Exception;	//사이드 사진 불러오기
-
-
-	//--------------------------------------------------硫붾돱
-
-	public void insert(MenuVO mvo) throws Exception;	//�엯�젰
-
-	public List<MenuVO> menuList() throws Exception;	//�쟾泥대낫湲�
-
-	public void delete(MenuVO mvo) throws Exception;	//�궘�젣
-
-	public MenuVO update(MenuVO mvo) throws Exception;	//�닔�젙
-
-	public MenuVO modno(String no) throws Exception;	//�젙蹂� �꽔湲�
-
-	public List<String> getFilename(String no) throws Exception;	//�궗吏� 遺덈윭�삤湲�
+	public List<String> getFilename(String no) throws Exception;	//메뉴 사진 불러오기
 
 }
