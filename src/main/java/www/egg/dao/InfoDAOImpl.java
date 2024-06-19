bece1cddf75a5fda6b459f25bb7ddd2338b6d38d
jasmine-dg <jasmine_design@naver.com>
2024-06-19 18:45:14 +0900

    jeong

--- src/main/java/www/egg/dao/InfoDAOImpl.java
+++ src/main/java/www/egg/dao/InfoDAOImpl.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import www.egg.vo.AskVO;
+import www.egg.vo.PageVO;
 
 @Repository
 public class InfoDAOImpl implements IF_InfoDAO {
@@ -18,12 +19,19 @@ public class InfoDAOImpl implements IF_InfoDAO {
 	private SqlSession sql;
 	
 	@Override
-	public void insert(AskVO avo) {
+	public void insert(AskVO avo) throws Exception {
 		sql.insert(mapperQurey + ".insert", avo);
 	}
 	
 	@Override
-	public List<AskVO> allList(String a_id) {
+	public List<AskVO> allList(String a_id) throws Exception {
+		
 		return sql.selectList(mapperQurey + ".selectall" , a_id);
+	}
+
+	@Override
+	public AskVO selectOne(Integer a_num) throws Exception {
+				
+		return sql.selectOne(mapperQurey + ".selectone", a_num);
 	}
 }
