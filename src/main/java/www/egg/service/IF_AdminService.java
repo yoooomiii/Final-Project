1382257a826e54c4483b93450c7b1830e7031eee
Kimeojin <eojin1914@gmail.com>
2024-06-24 09:28:41 +0900

    before hashmap paging

--- src/main/java/www/egg/service/IF_AdminService.java
+++ src/main/java/www/egg/service/IF_AdminService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import www.egg.vo.DeliveryVO;
-import www.egg.vo.MemberVO;
 import www.egg.vo.MlistVO;
 import www.egg.vo.PageVO;
 import www.egg.vo.PaymentVO;
